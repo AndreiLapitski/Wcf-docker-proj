1f347b100ffade3e2a95c8a40e2aae3987298668
Andrei Lapitski2 <Andrei_Lapitski2@epam.com>
2019-01-22 17:52:18 +0300

    new changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
-﻿using DAL.Models;
-using DAL.Repositories;
+﻿using Newtonsoft.Json;
 using System;
+using System.Collections;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace ConsoleApp1
 {
@@ -8,18 +10,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            TicketRepository repo = new TicketRepository();
-            //Ticket ticket = new Ticket();
-            //ticket.Number = 6;
+            using(var client = new HttpClient())
+            {              
+                client.BaseAddress = new Uri("http://localhost:51477");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            //repo.Create(ticket);
-            var list = repo.GetAll();
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
+                //method environment-variables
+                //var variables = client.GetAsync("environment-variables").Result;
+                //var deserVariables = JsonConvert.DeserializeObject<IDictionary>(variables.Content.ReadAsStringAsync().Result);
+
+                //foreach (var item in deserVariables.Keys)
+                //{
+                //    Console.WriteLine($"{item} -- {deserVariables[item]}");
+                //}
+
+                //method environment-variable/key
+                //HttpResponseMessage response = client.GetAsync("environment-variable/" + "USERNAME").Result;
+                //string value = response.Content.ReadAsStringAsync().Result;
+                //Console.WriteLine(value);
             }
 
             Console.Read();
         }
     }
 }
+//FPS_BROWSER_USER_PROFILE_STRING -- Default
+//LOGONSERVER -- \\EVBYGOMSA0003
+//USERNAME -- Andrei_Lapitski2
+//SystemRoot -- C:\WINDOWS
+//VSSKUEDITION -- Professional
+//MSMPI_BIN -- C:\Program Files\Microsoft MPI\Bin\
+//OneDrive -- C:\Users\Andrei_Lapitski2\OneDrive - EPAM
+//CommonProgramFiles(x86) -- C:\Program Files(x86)\Common Files
+//VSLANG -- 1033
+//UATDATA -- C:\Windows\CCM\UATData\D9F8C395-CAB8-491d-B8AC-179A1FE1BE77
+//IISEXPRESS_SITENAME -- WebApi
+//ProgramData -- C:\ProgramData
+//VSAPPIDDIR -- C:\Program Files(x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\
+//HOMEPATH -- \Users\Andrei_Lapitski2
+//_NO_DEBUG_HEAP -- 1
+//COMPUTERNAME -- EPBYGOMW0025
+//ALLUSERSPROFILE -- C:\ProgramData
+//DEV_ENVIRONMENT -- 1
+//IN_DEBUG_MODE -- 1
---
+++ WebApi/Controller/VariablesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Web.Http;
+
+namespace WebApi.Controller
+{
+    public class VariablesController : ApiController
+    {
+        [HttpGet]
+        [Route("environment-variables")]
+        public IDictionary EnvironmentVariables()
+        {
+            IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+            return environmentVariables;
+        }
+
+        [HttpGet]
+        [Route("environment-variable/{key}")]
+        public IHttpActionResult EnvironmentVariableByKey(string key)
+        {
+            string value = Environment.GetEnvironmentVariable(key);
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return BadRequest("Inroccect key");
+            }
+            
+            return Ok(value);
+        }
+    }
+}
